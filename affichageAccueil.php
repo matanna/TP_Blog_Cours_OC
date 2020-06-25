2fafc330c8d1ae3b200da6f1c1e01be6c0362e5b
Mathieu Bonhommeau <mathieu.bonhommeau@orange.fr>
2020-06-25 20:09:06 +0200

    Création du fichier modele.php pour isoler le traitement des données du reste

--- affichageAccueil.php
+++ affichageAccueil.php
@@ -11,18 +11,18 @@
         <h1>MON 1er BLOG</h1>
 
         <?php
-            while($billet_seul = $billets -> fetch())
+            while($donnees = $req -> fetch())
             {
         ?>
             <div class='post'>
-                <h3><span><?php echo $billet_seul['date_creation_fr']. '</span> : ' . htmlspecialchars($billet_seul['titre']); ?></h3>
-                <p><?php echo nl2br(htmlspecialchars($billet_seul['contenu'])); ?></p>
+                <h3><span><?php echo $donnees['date_creation_fr']. '</span> : ' . htmlspecialchars($donnees['titre']); ?></h3>
+                <p><?php echo nl2br(htmlspecialchars($donnees['contenu'])); ?></p>
                 
-                <p><a href="commentaire.php?id=<?php echo $billet_seul['id']; ?>">Lien vers les commentaires</a></p>
+                <p><a href="commentaire.php?id=<?php echo $donnees['id']; ?>">Lien vers les commentaires</a></p>
             </div>
         <?php
             }
-            $billets -> closeCursor();
+            $req -> closeCursor();
         ?>
 
     </body>
--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <?php
 
-try
-{
-    $bdd = new PDO('mysql:host=localhost;port=3308;dbname=blog;charset=utf8','root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-}
-catch (exception $e)
-{
-    die('Erreur : ' . $e->getMessage());
-}
-    
+require('modele.php');
 
-$billets = $bdd -> query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'le %d/%m/%Y à %Hh%im%ss \') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+$req =getBillets();
 
 require('affichageAccueil.php');
 
