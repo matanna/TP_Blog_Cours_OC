caa647bd13901bb3b315d1bc98ecf9cb5a5a5d46
Mathieu Bonhommeau <mathieu.bonhommeau@orange.fr>
2020-06-25 19:27:23 +0200

    séparation du code : Récup des données en 1ère partie et Affichage en 2è partie

--- index.php
+++ index.php
@@ -1,46 +1,51 @@
+<?php
+// Récupération des données
+
+try
+{
+    $bdd = new PDO('mysql:host=localhost;port=3308;dbname=blog;charset=utf8','root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch (exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+    
+
+$billets = $bdd -> query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'le %d/%m/%Y à %Hh%im%ss \') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+
+?>
+
+
+<!--Affichage-->
+
+
 <!DOCTYPE html>
 <html lang='fr'>
 
-<head>
-    <meta charset='utf-8' />
-    <title>Mon super blog </title>
-    <link rel="stylesheet" href="style.css" />
-</head>
+    <head>
+        <meta charset='utf-8' />
+        <title>Mon super blog </title>
+        <link rel="stylesheet" href="style.css" />
+    </head>
 
-<body>
-    <h1>MON 1er BLOG</h1>
+    <body>
+        <h1>MON 1er BLOG</h1>
 
+        <?php
+            while($billet_seul = $billets -> fetch())
+            {
+        ?>
+            <div class='post'>
+                <h3><span><?php echo $billet_seul['date_creation_fr']. '</span> : ' . htmlspecialchars($billet_seul['titre']); ?></h3>
+                <p><?php echo nl2br(htmlspecialchars($billet_seul['contenu'])); ?></p>
+                
+                <p><a href="commentaire.php?id=<?php echo $billet_seul['id']; ?>">Lien vers les commentaires</a></p>
+            </div>
+        <?php
+            }
+            $billets -> closeCursor();
+        ?>
 
-    <?php
-        //Connexion BDD
-        Try
-        {
-            $bdd = new PDO('mysql:host=localhost;port=3308;dbname=blog;charset=utf8','root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        }
-        catch (exception $e)
-        {
-            die('Erreur : ' . $e->getMessage());
-        }
-    
-
-        //Affichage des billets a l'aide d'une boucle
-        $billets = $bdd -> query('SELECT id, titre, contenu, date_creation FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
-
-        while($billet_seul = $billets -> fetch())
-        {
-    ?>
-        <div class='post'>
-            <h3><span><?php echo $billet_seul['date_creation']. '</span> : ' .$billet_seul['titre']; ?></h3>
-            <p><?php echo $billet_seul['contenu']; ?></p>
-            
-            <!--Passage en GET de l'id de chaque billet-->
-            <p><a href="commentaire.php?id=<?php echo $billet_seul['id']; ?>">Lien vers les commentaires</a></p>
-        </div>
-    <?php
-        }
-        $billets -> closeCursor();
-    ?>
-
-</body>
+    </body>
 
 </html>
